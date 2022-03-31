6b45bdba81d2d520159dc82452ddd960f1b43d6a
gracie2 <stevenmanangu360@gmail.com>
2022-03-31 10:18:05 +0300

    adding modal

---
+++ app/components/OptionModal.js
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet, Modal } from 'react-native'
+import React from 'react'
+
+const OptionModal = () => {
+ 
+    return (
+      <Modal visible={true}></Modal>
+    )
+  }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+export default OptionModal;
